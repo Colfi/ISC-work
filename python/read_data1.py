012b116246cf3871955224f40117de40864d9699
user01 <user01@teaching-980D.(none)>
2017-03-01 12:32:32 +0000

    Data reading and writing 1 and 2

---
+++ python/read_data1.py
@@ -0,0 +1,6 @@
+with open('weather.csv','r') as reader:
+ data=reader.read()
+print len(data)
+print data 
+
+
---
+++ python/read_data2.py
@@ -0,0 +1,7 @@
+with open('weather.csv','r') as reader:
+ line = reader.readline()
+ while line:
+  print line 
+  line = reader.readline()
+
+print 'It s over'

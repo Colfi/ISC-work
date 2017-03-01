4dcf6796247486dfb5dcd18212e0cd4a13ecb03e
user01 <user01@teaching-980D.(none)>
2017-03-01 11:40:23 +0000

    Added Tuples exercise 1 and 2

---
+++ python/tuples.py
@@ -0,0 +1,11 @@
+tuple1=1,
+print tuple1
+tuple2 = range(100,201)
+print tuple2
+
+mylist = [23,"hi",2.4e-10]
+for item in mylist:
+ print item,mylist.index(item)
+
+for item in mylist: enumerate(mylist)
+
